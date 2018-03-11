130286f72529dab1c269f7f2932fdacef1b424ea
Mohsen <mohkami.mohsen@gmail.com>
2018-03-11 16:30:27 -0700

    added the list using bootstrap groups and brought in the search input lib

--- src/App.js
+++ src/App.js
@@ -6,14 +6,6 @@ import {Row, Col} from 'react-bootstrap'
 import {Route} from 'react-router-dom'
 
 
-// <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h1 className="App-title">Welcome to React</h1>
-//         </header>
-//         <p className="App-intro">
-//           To get started, edit <code>src/App.js</code> and save to reload.
-//         </p>
-
 class App extends Component {
   render() {
     return (
--- src/navbar/NavBar.js
+++ src/navbar/NavBar.js
@@ -1,17 +1,45 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import {ListGroup, ListGroupItem} from 'react-bootstrap'
+import {LinkContainer} from 'react-router-bootstrap'
+import './NavBar.css'
+import SearchInput from 'react-search-input'
 
 class NavBar extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      searchTerm: ''
+    }
+    this.searchUpdated = this.searchUpdated.bind(this)
+  }
+
+  searchUpdated (term) {
+    this.setState({searchTerm: term})
+  }
+
   render() {
     return (
       <div>
         <div>
           This is the Nav Bar
         </div>
-        <ul>
-          <li> <Link to="/1">Contact 1</Link> </li>
-          <li> <Link to="/2">Contact 2</Link> </li>
-        </ul>
+        <div>
+        <SearchInput throttle={500} className="search-input" onChange={this.searchUpdated} />
+        </div>
+        <ListGroup>
+          <LinkContainer to="/1">
+            <ListGroupItem bsClass="list-group-item">
+              Contact 1
+            </ListGroupItem>
+          </LinkContainer>
+
+          <LinkContainer to="/2">
+            <ListGroupItem bsClass="list-group-item">
+              Contact 2
+            </ListGroupItem>
+          </LinkContainer>
+        </ListGroup>
       </div>
     );
   }
